b41a88d24fc6dd83875f418b9f6bf35e44db5207
jeyen10-cpu <jeyen10@gmail.com>
2026-08-24 10:19:23 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Header shadow on scroll
-const header = document.getElementById('siteHeader');
-window.addEventListener('scroll', () => {
-  header.classList.toggle('scrolled', window.scrollY > 10);
-});
